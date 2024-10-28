e374736dc93b52b21dd643ea80819c4c62a2860e
bigBrodyG <152636135+bigBrodyG@users.noreply.github.com>
2024-10-28 10:34:02 +0000

    rev1 28_10

---
+++ LEZ_28_10/main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+int e = 0;
+int maxum = 100;
+int main() {
+    for (size_t i = 0; i < (maxum); i++) {
+        e++;
+        cout << e;
+        printf(") Oggi ..........");
+        
+        printf("\n");
+    }
+    return 0;
+}
